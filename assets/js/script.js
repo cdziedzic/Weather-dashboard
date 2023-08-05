1a8b64344d5a7e6dd72da7b5b4bd103b0736ae1a
chris dziedzic <cdziedzic@gmail.com>
2023-08-05 11:32:38 -0400

    got both apis working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-
+let city = document.getElementById('city')
+let temp = document.getElementById('temp')
+let wind = document.getElementById('wind')
+let humid = document.getElementById('humidity')
 
 function getApi() {
-let requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=1ced4366f307f4ccdd38e8cd9911844f'
+let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=1ced4366f307f4ccdd38e8cd9911844f'
 
 fetch (requestUrl)
     .then(function (response) {
@@ -10,6 +13,24 @@ fetch (requestUrl)
 
     .then (function (data) {
         console.log(data)
+        console.log(data.list[0].main.temp)
+
+        city.textContent = data.city.name + data.list[0].weather.icon
+        temp.textContent = "Temperature: " + data.list[0].main.temp
+        wind.textContent = "Wind: " + data.list[0].wind.speed
+        humid.textContent = "Humidity: " + data.list[0].main.humidity
     });
 
+}
+
+function getCoords() {
+
+fetch ('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=1ced4366f307f4ccdd38e8cd9911844f')
+    .then(response => response.json())
+    .then(citiesFound => {
+        let firstCity = citiesFound[0]
+        console.log(citiesFound[0])
+        console.log(firstCity.lat);
+        console.log(firstCity.lon);
+    })
 }
