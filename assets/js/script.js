97cb2307feb6e16f91628359e8f692f3cef97dbb
chris dziedzic <cdziedzic@gmail.com>
2023-08-05 19:21:23 -0400

    pulled data input from button press and added on to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,35 +2,29 @@ let city = document.getElementById('city')
 let temp = document.getElementById('temp')
 let wind = document.getElementById('wind')
 let humid = document.getElementById('humidity')
+let submitBtn = document.getElementById('submit')
+let cityName = document.getElementById('cityName')
 
-function getApi() {
-let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=1ced4366f307f4ccdd38e8cd9911844f'
+submitBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+    let enteredText = cityName.value
 
-fetch (requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-
-    .then (function (data) {
-        console.log(data)
-        console.log(data.list[0].main.temp)
-
-        city.textContent = data.city.name + data.list[0].weather.icon
-        temp.textContent = "Temperature: " + data.list[0].main.temp
-        wind.textContent = "Wind: " + data.list[0].wind.speed
-        humid.textContent = "Humidity: " + data.list[0].main.humidity
-    });
-
-}
-
-function getCoords() {
-
-fetch ('http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=1ced4366f307f4ccdd38e8cd9911844f')
+    fetch (`http://api.openweathermap.org/geo/1.0/direct?q=${enteredText}&limit=5&units=imperial&appid=1ced4366f307f4ccdd38e8cd9911844f`)
     .then(response => response.json())
     .then(citiesFound => {
         let firstCity = citiesFound[0]
-        console.log(citiesFound[0])
-        console.log(firstCity.lat);
-        console.log(firstCity.lon);
+   
+
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${firstCity.lat}&lon=${firstCity.lon}&appid=1ced4366f307f4ccdd38e8cd9911844f`)
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.list[0].main.humidity)
+                city.textContent = data.city.name + data.list[0].weather.icon;
+                temp.textContent = "Temperature: " + data.list[0].main.temp;
+                wind.textContent = "Wind: " + data.list[0].wind.speed;
+                humid.textContent = "Humidity: " + data.list[0].main.humidity +"%";
     })
-}
+});
+   
+
